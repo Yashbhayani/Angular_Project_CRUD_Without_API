71e90fc69e043b90f7bdace30475de50415ecf5b
Yash Bhayani <83011938+Yashbhayani@users.noreply.github.com>
2022-04-05 11:59:55 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,16 +14,11 @@ export class AppComponent {
   user = new Array();
 
   userprofileForm = new FormGroup({
-    //  id: new FormControl(this.user.length),
     fname: new FormControl('', Validators.required),
     email: new FormControl('', Validators.required),
   });
 
   onSubmit() {
-    /*console.warn(this.userprofileForm.value);
-      let id = this.userprofileForm.get('id')?.value;
-      let userLenth = this.user.length;
-      this.userprofileForm.controls['id'].setValue(userLenth + 1);*/
       if (this.case == 0) {
         this.user.push(this.userprofileForm.value);
       } else {
@@ -37,23 +32,10 @@ export class AppComponent {
   }
 
   removelist(sd: number) {
-    /*this.user = this.user.filter(item => item.sd!= sd);
-      this.user = this.user.filter(item => item.sd - 1 != sd - 1);
-      console.warn(sd);*/
     this.user.splice(sd, 1);
-    /*this.user = this.user.filter(item => item.sd!= sd);
-      console.warn(this.user[sd - 1]);
-      console.warn(this.user.slice(sd-1,1));
-      console.warn(this.user.slice(sd-1,1));*/
   }
 
   editlist(sd: number) {
-    /*console.warn(this.user[sd-1]);
-    this.userprofileForm.controls['id'].setValue(this.user[sd - 1].id);
-    this.userprofileForm.controls['fname'].setValue(this.user[sd - 1].fname);
-    this.userprofileForm.controls['email'].setValue(this.user[sd - 1].email);
-    console.warn(this.user[sd])
-    this.userprofileForm.controls['id'].setValue(this.user[sd].id);*/
     this.userprofileForm.controls['fname'].setValue(this.user[sd].fname);
     this.userprofileForm.controls['email'].setValue(this.user[sd].email);
     this.YP = this.user[sd];
